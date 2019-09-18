ad696e06bcff0c8c390cb368e56873139478ce3c
daxterbels <daxter1987@gmail.com>
2019-09-18 14:40:14 -0400

    fixing name

--- src/AmazonS3HelperProvider.php
+++ src/AmazonS3HelperProvider.php
@@ -3,7 +3,7 @@
 namespace daxter1987\AmazonS3Helper;
 use Illuminate\Support\ServiceProvider;
 
-class AmazonS3Helper extends ServiceProvider {
+class AmazonS3HelperProvider extends ServiceProvider {
 
     public function boot()
     {
